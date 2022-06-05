696fe0344697478727925c6e81670c149b2eef20
Tobias <tobias.leibelt@fau.de>
2022-06-05 16:21:05 +0200

    See changes in Word File in Teams

--- run.py
+++ run.py
@@ -25,13 +25,13 @@ except KeyError:
     exit('Error: Invalid <config_mode>. Expected values [Debug, Production] ')
 
 app = create_app(app_config)
-Migrate(app, db)
+#Migrate(app, db)
 
-if DEBUG:
-    app.logger.info('DEBUG       = ' + str(DEBUG)             )
-    app.logger.info('FLASK_ENV   = ' + os.getenv('FLASK_ENV') )
-    app.logger.info('DBMS        = ' + app_config.SQLALCHEMY_DATABASE_URI)
-    app.logger.info('ASSETS_ROOT = ' + app_config.ASSETS_ROOT )
+#if DEBUG:
+#    app.logger.info('DEBUG       = ' + str(DEBUG)             )
+#    app.logger.info('FLASK_ENV   = ' + os.getenv('FLASK_ENV') )
+#    app.logger.info('DBMS        = ' + app_config.SQLALCHEMY_DATABASE_URI)
+#    app.logger.info('ASSETS_ROOT = ' + app_config.ASSETS_ROOT )
 
 if __name__ == "__main__":
     app.run()
